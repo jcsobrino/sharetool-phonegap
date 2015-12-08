e5ac9656fdb18a804b55369e7ab0dd43c041e1ee
JoséCarlos <ss@ss.com>
2015-12-08 11:35:13 +0100

    Validations

--- www/js/sharetool-init.js
+++ www/js/sharetool-init.js
@@ -47,8 +47,87 @@ sharetoolApp.run(function($ionicPlatform, apiService) {
         if(window.StatusBar) {
             StatusBar.styleDefault();
         }
-        
-        
-        
     });
 });
+
+
+sharetoolApp.factory('ionicElementModifier', [
+    function () {
+        var /**
+        * @ngdoc function
+        * @name myCustomElementModifier#makeValid
+        * @methodOf myCustomElementModifier
+        *
+        * @description
+        * Makes an element appear valid by apply custom styles and child elements.
+        *
+        * @param {Element} el - The input control element that is the target of the validation.
+        */
+        makeValid = function (el) {
+        	reset(el);
+        },
+        
+        reset = function (el) {
+        	el = el.parent();
+	        angular.forEach(el.find('span'), function (spanEl) {
+	          spanEl = angular.element(spanEl);
+	          if (spanEl.hasClass('error-msg')) {
+	            spanEl.remove();
+	          }
+	        });
+	
+	        el.removeClass('has-success has-error has-feedback');
+        },        
+        insertAfter = function (referenceNode, newNode) {
+            referenceNode[0].parentNode.insertBefore(newNode[0], referenceNode[0].nextSibling);
+        },
+
+        /**
+        * @ngdoc function
+        * @name myCustomElementModifier#makeInvalid
+        * @methodOf myCustomElementModifier
+        *
+        * @description
+        * Makes an element appear invalid by apply custom styles and child elements.
+        *
+        * @param {Element} el - The input control element that is the target of the validation.
+        * @param {String} errorMsg - The validation error message to display to the user.
+        */
+        makeInvalid = function (el, errorMsg) {
+        	reset(el);
+        	var helpTextEl = angular.element('<span class="error-msg">' + errorMsg + '</span>');
+        	insertAfter(el, helpTextEl);
+        	console.log(errorMsg);
+        },
+
+
+        /**
+        * @ngdoc function
+        * @name myCustomElementModifier#makeDefault
+        * @methodOf myCustomElementModifier
+        *
+        * @description
+        * Makes an element appear in its default visual state.
+        *
+        * @param {Element} el - The input control element that is the target of the validation.
+        */
+        makeDefault = function (el) {
+            // return the element to a default visual state i.e. before any form of validation was applied
+        };
+
+        return {
+            makeValid: makeValid,
+            makeInvalid: makeInvalid,
+            makeDefault: makeDefault,
+            key: 'ionicElementModifierKey'
+        };
+    }
+]);
+
+sharetoolApp.run(['validator', 'ionicElementModifier', function (validator, ionicElementModifier) {
+    validator.registerDomModifier(ionicElementModifier.key, ionicElementModifier);
+    validator.setDefaultElementModifier(ionicElementModifier.key);
+	}
+]);
+
+
